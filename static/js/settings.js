8646780819c90eb2571e6c8510c23446dbd4f0c6
nikhil.shikarkhane <shikarkhane@gmail.com>
2014-06-26 15:18:07 +0000

    added slider for filtering days

--- static/js/settings.js
+++ static/js/settings.js
@@ -8,18 +8,6 @@ function change_vote_label( flag ){
 }
 
 $(function() {
-        $( "#search-radius-slider" ).slider({
-          range: "max",
-          min: 1,
-          max: 9,
-          value: $.cookie('mysearchradius'),
-          slide: function( event, ui ) {
-            $( "#search-radius-label" ).text( ui.value );
-            $.cookie('mysearchradius',  ui.value, { path: '/'});
-          }
-        });
-        $( "#search-radius-label" ).text( $.cookie('mysearchradius'));
-
         $( "#sort-by-votes-slider" ).slider({
           range: "max",
           min: 0,
@@ -32,5 +20,29 @@ $(function() {
           }
         });
         change_vote_label( $.cookie('mysortbyvotes'));
+
+        $( "#filter-days-slider" ).slider({
+          range: "max",
+          min: 1,
+          max: 30,
+          value: $.cookie('myfilterdays'),
+          slide: function( event, ui ) {
+            $( "#filter-days-label" ).text( ui.value );
+            $.cookie('myfilterdays',  ui.value, { path: '/'});
+          }
+        });
+        $( "#filter-days-label" ).text( $.cookie('myfilterdays'));
+
+        $( "#search-radius-slider" ).slider({
+          range: "max",
+          min: 1,
+          max: 9,
+          value: $.cookie('mysearchradius'),
+          slide: function( event, ui ) {
+            $( "#search-radius-label" ).text( ui.value );
+            $.cookie('mysearchradius',  ui.value, { path: '/'});
+          }
+        });
+        $( "#search-radius-label" ).text( $.cookie('mysearchradius'));
 
   });
